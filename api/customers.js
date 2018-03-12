2cf76ff11423fcfcb3b4177fa491f23b1a6866d7
louiaR <louitiani@gmail.com>
2018-03-12 16:19:45 +0000

    modify api/customer.js

---
+++ api/customers.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+
+const customers = require("../public/data/customers.json");
+
+router.get('/api/customers', (req, res) =>{
+  res.status(200).json(customers)
+})
+
+router.get("/api/customers/:id?", (req, res) => {
+  console.log(re.params.id)
+  if (req.params.id) {
+    const filter = customers.filter(
+      customer => customer.id === parseInt(req.params.id)
+    );
+    res.status(200).json({
+      customers: filter
+    });
+  } else {
+    res.status(200).json({
+      customers: customers
+    });
+  }
+});
+
+router.post("/", (req, res) => {
+  // TODO read req.body.customer
+  res.status(200).json(req.body.customer);
+});
+
+router.put("/", (req, res) => {
+  // TODO update per req.body.customer
+  res.status(200).json(req.body.customer);
+});
+
+router.delete("/", (req, res) => {
+  // TODO delete req.body.customer
+  res.status(200).json(req.body.customer);
+});
+
+module.exports = router;
